b9560e6efae5b0271d27db6dc3a266382978edcc
mehmeow <syarezan.im@gmail.com>
2021-09-16 21:57:27 +0800

    dep dotenv

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+console.log('process.env.USER_ID',{a:process.env.PGUSER, b:process.env.USER_ID});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const { Pool, Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// get db
+router.get('/db', function (req, res, next) {
+  client.connect();
+  client.query('SELECT * FROM products;', (err, res) => {
+    console.log('res',{DB_URL:process.env.USER_ID,err,res});
+    // if (err) throw err;
+    // for (let row of res.rows) {
+    //   console.log('row', JSON.stringify(row));
+    // }
+    client.end();
+  });
+  res.status(200).send({ test: 'ok lah' });
 });
 
 module.exports = router;
